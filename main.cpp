1ac96c6fe475f0c5a7e5d58ba8a6cddbcab5c109
Quyse Lert <quyse0@gmail.com>
2012-12-31 22:40:18 +0600

    working triangle version

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include "../inanity2/inanity-shaders.hpp"
 #include <sstream>
 #include <iostream>
+#include <fstream>
 
 #define TEST_GRAPHICS_DIRECTX
 //#define TEST_GRAPHICS_OPENGL
@@ -19,12 +20,11 @@ struct Vertex
 	float2 texcoord;
 };
 
-class TestShader
+struct TestShader
 {
-private:
-	Value<float4> aPosition;
-	Value<float3> aNormal;
-	Value<float2> aTexcoord0;
+	Attribute<float4> aPosition;
+	Attribute<float3> aNormal;
+	Attribute<float2> aTexcoord;
 
 	ptr<UniformGroup> ugCamera;
 	Uniform<float4x4> uViewProj;
@@ -41,27 +41,30 @@ private:
 	Uniform<float4x4> uWorldViewProj;
 	Uniform<float4x4> uWorld;
 
-public:
 	TestShader() :
-		aPosition(NEW(AttributeNode(0, DataTypes::Float4))),
-		aNormal(NEW(AttributeNode(12, DataTypes::Float3))),
-		aTexcoord0(NEW(AttributeNode(24, DataTypes::Float2))),
+		aPosition(0),
+		aNormal(1),
+		aTexcoord(2),
 
-		ugCamera(NEW(UniformGroup())),
+		ugCamera(NEW(UniformGroup(0))),
 		uViewProj(ugCamera->AddUniform<float4x4>()),
 		uCameraPosition(ugCamera->AddUniform<float3>()),
 
-		ugLight(NEW(UniformGroup())),
+		ugLight(NEW(UniformGroup(1))),
 		uLightPosition(ugLight->AddUniform<float3>()),
 		uLightDirection(ugLight->AddUniform<float3>()),
 
-		ugMaterial(NEW(UniformGroup())),
+		ugMaterial(NEW(UniformGroup(2))),
 		uDiffuseSampler(0),
 
-		ugModel(NEW(UniformGroup())),
+		ugModel(NEW(UniformGroup(3))),
 		uWorldViewProj(ugModel->AddUniform<float4x4>()),
 		uWorld(ugModel->AddUniform<float4x4>())
 	{
+		ugCamera->Finalize();
+		ugLight->Finalize();
+		ugMaterial->Finalize();
+		ugModel->Finalize();
 	}
 };
 
@@ -79,6 +82,11 @@ public:
 	{
 		float color[4] = { 1, 0, 0, 0 };
 		context->ClearRenderBuffer(presenter->GetBackBuffer(), color);
+
+		context->Reset();
+		context->GetTargetState() = drawingState;
+		context->Draw();
+
 		presenter->Present();
 	}
 
@@ -105,15 +113,69 @@ public:
 
 		// разметка
 		std::vector<Layout::Element> layoutElements;
-		layoutElements.push_back(Layout::Element(DataTypes::Float3, 0, Semantics::CustomPosition));
-		layoutElements.push_back(Layout::Element(DataTypes::Float3, 12, Semantics::CustomNormal));
-		layoutElements.push_back(Layout::Element(DataTypes::Float2, 24, Semantics::CustomTexcoord0));
+		layoutElements.push_back(Layout::Element(DataTypes::Float3, 0, 0));
+		layoutElements.push_back(Layout::Element(DataTypes::Float3, 12, 1));
+		layoutElements.push_back(Layout::Element(DataTypes::Float2, 24, 2));
 		ptr<Layout> layout = NEW(Layout(layoutElements, sizeof(Vertex)));
 
 		// шейдер :)
-		TestShader testShader;
+		TestShader t;
 
+		Interpolant<float4> tPosition(Semantics::VertexPosition);
+		Interpolant<float3> tNormal(Semantics::CustomNormal);
+		Interpolant<float2> tTexcoord(Semantics::CustomTexcoord0);
+
+		Temp<float4> tmpPosition;
+		Temp<float3> tmpNormal;
+
+		Fragment<float4> tTarget(Semantics::TargetColor0);
+
+		Expression vertexShader = (
+			tPosition = t.aPosition
+			);
+
+		Expression pixelShader = (
+				tTarget = newfloat4(0, 1, 0, 1)
+			);
+
+		ptr<HlslGenerator> shaderGenerator = NEW(HlslGenerator());
+		ptr<ShaderSource> vertexShaderSource = shaderGenerator->Generate(vertexShader, ShaderTypes::vertex);
+		ptr<ShaderSource> pixelShaderSource = shaderGenerator->Generate(pixelShader, ShaderTypes::pixel);
+
+		ptr<DxShaderCompiler> shaderCompiler = NEW(DxShaderCompiler());
+		ptr<File> vertexShaderBinary = shaderCompiler->Compile(vertexShaderSource);
+		ptr<File> pixelShaderBinary = shaderCompiler->Compile(pixelShaderSource);
+
+		ptr<FileSystem> fs = FolderFileSystem::GetNativeFileSystem();
+		fs->SaveFile(vertexShaderSource->GetCode(), "vs.fx");
+		fs->SaveFile(pixelShaderSource->GetCode(), "ps.fx");
+		fs->SaveFile(vertexShaderBinary, "vs.fxo");
+		fs->SaveFile(pixelShaderBinary, "ps.fxo");
+
+		float4x4 viewMatrix = CreateLookAtMatrix(float3(100, 100, 100), float3(0, 0, 0), float3(0, 0, 1));
+		t.uWorldViewProj.SetValue(viewMatrix);
+
+		drawingState.viewportWidth = mode.width;
+		drawingState.viewportHeight = mode.height;
 		drawingState.renderBuffers[0] = presenter->GetBackBuffer();
+		drawingState.vertexShader = device->CreateVertexShader(vertexShaderBinary);
+		drawingState.pixelShader = device->CreatePixelShader(pixelShaderBinary);
+
+#if 0
+		ptr<File> vertexBufferFile = NEW(MemoryFile(sizeof(Vertex) * 3));
+		Vertex* vertexBufferData = (Vertex*)vertexBufferFile->GetData();
+		vertexBufferData[0].position = float3(-0.5f, -0.5f, 0);
+		vertexBufferData[1].position = float3(0, 0.5f, 0);
+		vertexBufferData[2].position = float3(0.5, 0, 0);
+		drawingState.vertexBuffer = device->CreateVertexBuffer(vertexBufferFile, layout);
+		ptr<File> indexBufferFile = NEW(MemoryFile(sizeof(short) * 3));
+		short* indexBufferData = (short*)indexBufferFile->GetData();
+		indexBufferData[0] = 0;
+		indexBufferData[1] = 1;
+		indexBufferData[2] = 2;
+		drawingState.indexBuffer = device->CreateIndexBuffer(indexBufferFile, sizeof(short));
+#else
+#endif
 
 		window->Run(Win32Window::ActiveHandler::CreateDelegate(MakePointer(this), &Game::onTick));
 	}
---
+++ run.bat
@@ -0,0 +1,3 @@
+set NOPROGRESS=1
+call ice-cpp exe:debug/trashgame
+debug\trashgame.exe
