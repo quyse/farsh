c4a4a3a77a733dac851c56eaf73dc3b0f5c6e09f
Quyse Lert <quyse0@gmail.com>
2013-11-27 08:03:50 +0000

    fix paths in main script

--- main.lua
+++ main.lua
@@ -7,7 +7,7 @@ game:SetAmbient(0.02, 0.02, 0.02)
 
 -- материал кровищи
 local matBlood = Farsh.Material()
---matBlood:SetDiffuseTexture(game:LoadTexture("blood.png"))
+--matBlood:SetDiffuseTexture(game:LoadTexture("/blood.png"))
 matBlood:SetDiffuse({1, 0, 0, 1})
 matBlood:SetSpecular({0.2, 0, 0, 0})
 game:SetDecalMaterial(matBlood)
@@ -15,9 +15,9 @@ game:SetDecalMaterial(matBlood)
 -- материал кубика
 local matCube = Farsh.Material()
 t.matCube = matCube
---matCube:SetDiffuseTexture(game:LoadTexture("blood.png"))
+--matCube:SetDiffuseTexture(game:LoadTexture("/blood.png"))
 matCube:SetDiffuse({1, 1, 1, 1})
---matCube:SetSpecularTexture(game:LoadTexture("specular.jpg"))
+--matCube:SetSpecularTexture(game:LoadTexture("/specular.jpg"))
 matCube:SetSpecular({0.2, 0, 0, 0})
 
 local CreateStaticRigidModel = function(geometry, material, physicsShape, x, y, z)
@@ -29,15 +29,15 @@ local matBench = Farsh.Material()
 t.matBench = matBench
 matBench:SetDiffuse({1, 1, 1, 1})
 matBench:SetSpecular({0.1, 0.1, 0.1, 0.2})
-game:AddStaticModel(game:LoadGeometry("bench.geo"), matBench, 0, 2, 0)
+game:AddStaticModel(game:LoadGeometry("/bench.geo"), matBench, 0, 2, 0)
 
 local matNescafe = Farsh.Material()
-local texNescafe = game:LoadTexture("nescafe.png")
+local texNescafe = game:LoadTexture("/nescafe.png")
 matNescafe:SetDiffuseTexture(texNescafe)
 matNescafe:SetSpecularTexture(texNescafe)
-game:AddStaticModel(game:LoadGeometry("nescafe.geo"), matNescafe, 2, 0, 0)
+game:AddStaticModel(game:LoadGeometry("/nescafe.geo"), matNescafe, 2, 0, 0)
 
-local geoCube = game:LoadGeometry("box.geo")
+local geoCube = game:LoadGeometry("/box.geo")
 local shapeCube = game:CreatePhysicsBoxShape(1, 1, 1)
 game:CreatePhysicsRigidBody(game:CreatePhysicsBoxShape(10, 10, 1), 0, 11, 11, -1)
 for i = 1, 10 do
@@ -70,27 +70,27 @@ light2:SetShadow(true)
 
 -- установка параметров
 
-local zhDiffuse = game:LoadTexture("zombie_d.png")
-local zhSpecular = game:LoadTexture("zombie_s.png")
+local zhDiffuse = game:LoadTexture("/zombie_d.png")
+local zhSpecular = game:LoadTexture("/zombie_s.png")
 
 local zombieMaterial = Farsh.Material()
 zombieMaterial:SetDiffuseTexture(zhDiffuse)
 zombieMaterial:SetSpecularTexture(zhSpecular)
-local zombieGeometry = game:LoadSkinnedGeometry("zombie.geo")
-local zombieSkeleton = game:LoadSkeleton("zombie.skeleton")
-game:SetZombieParams(zombieMaterial, zombieGeometry, zombieSkeleton, game:LoadBoneAnimation("zombie.ba", zombieSkeleton))
+local zombieGeometry = game:LoadSkinnedGeometry("/zombie.geo")
+local zombieSkeleton = game:LoadSkeleton("/zombie.skeleton")
+game:SetZombieParams(zombieMaterial, zombieGeometry, zombieSkeleton, game:LoadBoneAnimation("/zombie.ba", zombieSkeleton))
 
-game:SetHeroParams(zombieMaterial, zombieGeometry, zombieSkeleton, game:LoadBoneAnimation("hero.ba", zombieSkeleton))
+game:SetHeroParams(zombieMaterial, zombieGeometry, zombieSkeleton, game:LoadBoneAnimation("/hero.ba", zombieSkeleton))
 
 local axeMaterial = Farsh.Material()
-axeMaterial:SetDiffuseTexture(game:LoadTexture("axe_d.png"))
+axeMaterial:SetDiffuseTexture(game:LoadTexture("/axe_d.png"))
 --axeMaterial:SetSpecularTexture(game:LoadTexture("axe_s.png"))
 axeMaterial:SetSpecular({0.5, 0, 0, 0})
-game:SetAxeParams(axeMaterial, game:LoadGeometry("axe.geo"), game:LoadBoneAnimation("axe.ba", nil))
+game:SetAxeParams(axeMaterial, game:LoadGeometry("/axe.geo"), game:LoadBoneAnimation("/axe.ba", nil))
 
 local circularMaterial = Farsh.Material()
-circularMaterial:SetDiffuseTexture(game:LoadTexture("circular_d.png"))
-circularMaterial:SetSpecularTexture(game:LoadTexture("circular_s.png"))
-game:SetCircularParams(circularMaterial, game:LoadGeometry("circular.geo"), game:LoadBoneAnimation("circular.ba", nil))
+circularMaterial:SetDiffuseTexture(game:LoadTexture("/circular_d.png"))
+circularMaterial:SetSpecularTexture(game:LoadTexture("/circular_s.png"))
+game:SetCircularParams(circularMaterial, game:LoadGeometry("/circular.geo"), game:LoadBoneAnimation("/circular.ba", nil))
 
 game:PlaceHero(10, 10, 10)
