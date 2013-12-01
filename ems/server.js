65c5ae4630eea509e8a3e224d87b21092532671a
Quyse Lert <quyse0@gmail.com>
2013-12-01 13:50:56 +0000

    support debug/release builds in ems server

--- ems/server.js
+++ ems/server.js
@@ -1,20 +1,29 @@
 var express = require('express');
 var fs = require('fs');
 
-var initialize = function(app, secure) {
+var initialize = function(app) {
 	app.configure(function() {
 		app.use(express.logger());
 		app.use(express.favicon());
 		app.use(express.bodyParser());
 		app.use(express.cookieParser());
 		app.use(app.router);
-		app.use(express.static(__dirname + '/../debug', {
+		app.use('/debug', express.static(__dirname + '/../debug', {
 			maxAge: 0
 		}));
-		app.use('/assets', express.static(__dirname + '/../assets', {
+		app.use('/debug/assets', express.static(__dirname + '/../assets', {
 			maxAge: 0
 		}));
-		app.use(express.static(__dirname + '/site', {
+		app.use('/debug', express.static(__dirname + '/site', {
+			maxAge: 0
+		}));
+		app.use('/release', express.static(__dirname + '/../release', {
+			maxAge: 0
+		}));
+		app.use('/release/assets', express.static(__dirname + '/../assets', {
+			maxAge: 0
+		}));
+		app.use('/release', express.static(__dirname + '/site', {
 			maxAge: 0
 		}));
 	});
@@ -22,4 +31,4 @@ var initialize = function(app, secure) {
 	return app;
 };
 
-initialize(express(), false).listen(8080);
+initialize(express()).listen(8080, '0.0.0.0');
