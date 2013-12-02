672c31ce9ebff0d769d711dafefe31cf67e117b9
Quyse Lert <quyse0@gmail.com>
2013-12-02 19:21:28 +0000

    change format of skinned geometry

--- GeometryFormats.cpp
+++ GeometryFormats.cpp
@@ -9,12 +9,12 @@ GeometryFormats::GeometryFormats() :
 	aleNormal(al->AddElement(als, vl->AddElement(DataTypes::_vec3, 12))),
 	aleTexcoord(al->AddElement(als, vl->AddElement(DataTypes::_vec2, 24))),
 
-	vlSkinned(NEW(VertexLayout(64))),
+	vlSkinned(NEW(VertexLayout(52))),
 	alSkinned(NEW(AttributeLayout())),
 	alsSkinned(alSkinned->AddSlot()),
 	aleSkinnedPosition(alSkinned->AddElement(alsSkinned, vlSkinned->AddElement(DataTypes::_vec3, 0))),
 	aleSkinnedNormal(alSkinned->AddElement(alsSkinned, vlSkinned->AddElement(DataTypes::_vec3, 12))),
 	aleSkinnedTexcoord(alSkinned->AddElement(alsSkinned, vlSkinned->AddElement(DataTypes::_vec2, 24))),
-	aleSkinnedBoneNumbers(alSkinned->AddElement(alsSkinned, vlSkinned->AddElement(DataTypes::_uvec4, 32))),
-	aleSkinnedBoneWeights(alSkinned->AddElement(alsSkinned, vlSkinned->AddElement(DataTypes::_vec4, 48)))
+	aleSkinnedBoneNumbers(alSkinned->AddElement(alsSkinned, vlSkinned->AddElement(DataTypes::_uvec4, LayoutDataTypes::Uint8, 32))),
+	aleSkinnedBoneWeights(alSkinned->AddElement(alsSkinned, vlSkinned->AddElement(DataTypes::_vec4, 36)))
 {}
